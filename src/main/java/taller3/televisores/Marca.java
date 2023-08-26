fe2d42e3b47ce65de476a3ef5f028b43ad8f0e91
damazog <damazog@unal.edu.co>
2023-08-26 01:41:34 -0500

    de todito

---
+++ src/main/java/taller3/televisores/Marca.java
@@ -0,0 +1,17 @@
+package taller3.televisores;
+
+public class Marca {
+	private String nombre;
+	
+	public Marca (String nombre) {
+		this.nombre=nombre;
+	}
+	
+	public String getMarca() {
+		return this.Marca
+	}
+	
+	public void setMarca(String nombre) {
+		this.nombre=nombre;
+	}
+}
